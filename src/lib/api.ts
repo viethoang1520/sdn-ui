0fa10e730ed1723cfd1ab26a55ab9f976169725f
nguyenbachduyhoang <nguyenduyhoang1494@gmail.com>
2025-06-21 23:24:36 +0700

    Tickets01

---
+++ src/lib/api.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export async function purchaseTicket(data: any) {
+  const res = await axios.post(`${API_URL}/purchase/ticket`, data);
+  return res.data;
+}
