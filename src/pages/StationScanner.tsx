9716c61292adc182f92ef17baa26684dcf76352b
Bao Trong <nhbaotrong@gmail.com>
2025-07-10 19:39:42 +0700

    feat: generate qr code for view ticket

---
+++ src/pages/StationScanner.tsx
@@ -0,0 +1,14 @@
+// import React, { useEffect } from 'react'
+// import { Html5Qrcode } from 'html5-qrcode';
+
+export default function StationScanner() {
+     
+     return (
+          <div>
+               <div>
+                    <h2>Scan QR Code</h2>
+                    <div id="qr-reader" style={{ width: "300px", height: "300px" }}></div>
+               </div>
+          </div>
+     )
+}
